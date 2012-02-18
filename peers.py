bcd8f6678f55bb1742c85d39aba73258c0013514
Alireza Nourian <alireza.nournia@gmail.com>
2012-02-18 23:44:40 +0330

    add request message

--- peers.py
+++ peers.py
@@ -21,17 +21,17 @@ def segmentFile(file):
 	return segments
 
 class TorrentAgent(spade.Agent.Agent):
-	def __init__(self, name, want, have):
+	def __init__(self, name, want, have, bwUp, bwDown):
+		self._name, self._bwUp, self._bwDown = name, bwUp, bwDown
 		self._segments = have # sha1 indexed strings
-		self._name = name # string
 		self._have = have.keys() # segment list
 		self._want = want # segment list
 		self._interest = [] # segment list
 
 		# have: sha1 indexed lists of peers
 		# interest: which peer wants which segments
-		# unchoke: list of unchoked peers
-		self._others = {'have': {}, 'interest': {}, 'unchoke': []}
+		# unchoked: list of unchoked peers
+		self._others = {'have': {}, 'interest': {}, 'unchoked': []}
 
 		super(TorrentAgent, self).__init__(getAddress(self._name), "secret")
 		self.wui.start()
@@ -45,6 +45,26 @@ class TorrentAgent(spade.Agent.Agent):
 		template = spade.Behaviour.ACLTemplate()
 		template.addReceiver(spade.AID.aid(getAddress(self._name), ['xmpp://'+ getAddress(self._name)]))
 		self.addBehaviour(self.ReceiveBehaviour(), spade.Behaviour.MessageTemplate(template))
+
+	def getPeerPieces(self, peer):
+		pieces = []
+		for piece, peers in self._others['have'].items():
+			if peer in peers:
+				pieces.append(piece)
+		return pieces
+
+	def sendMsg(self, receiver, content, kind = 'inform'):
+		msg = spade.ACLMessage.ACLMessage(kind)
+		msg.addReceiver(spade.AID.aid(getAddress(receiver), ['xmpp://'+ getAddress(receiver)]))
+		msg.setContent(content)
+		self.send(msg)
+
+	def replyMsg(self, msg, content, kind = 'inform'):
+		rep = msg.createReply()
+		rep.setPerformative(kind)
+		rep.setContent(content)
+		self.send(rep)
+
 
 	class ReceiveBehaviour(spade.Behaviour.EventBehaviour):
 		def _process(self):
@@ -64,17 +84,24 @@ class TorrentAgent(spade.Agent.Agent):
 				# print sender, 'to', agent._name, ':', content
 				if content['type'] == 'tracker':
 					agent._others['have'] = content['have']
+
 				elif content['type'] == 'interest':
 					addToDict(agent._others['interest'], sender, content['segment'])
+
 				elif content['type'] == 'unchoke':
-					print sender, 'to', agent._name, ':', content
+					pieces = agent.getPeerPieces(sender)
+					agent.replyMsg(msg, {'type': 'request', 'piece': random.choice(pieces), 'bw': bwDown}, 'request')
+
+				elif content['type'] == 'request':
+					print sender, 'requests', agent._name, ':', content
+
 
 	class TorrentBehaviour(spade.Behaviour.OneShotBehaviour):
 		def _process(self):
 			agent = self.myAgent
 
 			# ask tracker
-			self.sendMsg('tracker', {'have': agent._have})
+			agent.sendMsg('tracker', {'have': agent._have})
 
 			while 1:
 				try:
@@ -83,24 +110,18 @@ class TorrentAgent(spade.Agent.Agent):
 						segment = random.choice(agent._want)
 						if segment in agent._others['have']:
 							peer = random.choice(agent._others['have'][segment])
-							self.sendMsg(peer, {'type': 'interest', 'segment': segment})
+							agent.sendMsg(peer, {'type': 'interest', 'segment': segment})
 							agent._interest.append(segment)
 							agent._want.remove(segment)
 
 					# send uchocke message
 					if len(agent._others['interest']) > 0:
-						peer = random.choice(list(set(agent._others['interest'].keys()) ^ set(agent._others['unchoke'])))
-						self.sendMsg(peer, {'type': 'unchoke'})
-						agent._others['unchoke'].append(peer)
+						peer = random.choice(list(set(agent._others['interest'].keys()) ^ set(agent._others['unchoked'])))
+						agent.sendMsg(peer, {'type': 'unchoke', 'bw': bwUp})
+						agent._others['unchoked'].append(peer)
 
 				except: pass
 				time.sleep(1)
-
-		def sendMsg(self, receiver, content, kind = 'inform'):
-			msg = spade.ACLMessage.ACLMessage(kind)
-			msg.addReceiver(spade.AID.aid(getAddress(receiver), ['xmpp://'+ getAddress(receiver)]))
-			msg.setContent(content)
-			self.myAgent.send(msg)
 
 # execution
 if __name__ == "__main__":
@@ -116,12 +137,13 @@ if __name__ == "__main__":
 		else:
 			want = segments.keys(); have = {}
 
-		peers.append(TorrentAgent('peer' + str(i), want, have))
+		bwUp, bwDown = 500, 1000
+		peers.append(TorrentAgent('peer' + str(i), want, have, bwUp, bwDown))
 		# peers[i].setDebugToScreen()
 
 	alive = True
 	while alive:
-		try: time.sleep(1) 
+		try: time.sleep(1)
 		except KeyboardInterrupt: alive=False
 	
 	for i in range(peer_count):
